e6027fe2063b7bbe0a33778cf7b4785e517bf85e
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-09-14 13:00:31 -0700

    Javascript working, css needs work and so does banner

---
+++ src/images.js
@@ -0,0 +1,30 @@
+import applegreen from './images/applegreen.png';
+import blue from './images/blue.png';
+import brown from './images/brown.png';
+import cyan from './images/cyan.png';
+import forestgreen from './images/forestgreen.png';
+import gold from './images/gold.png';
+import green from './images/green.png';
+import grey from './images/grey.png';
+import mossgreen from './images/mossgreen.png';
+import purple from './images/purple.png';
+import red from './images/red.png';
+import teal from './images/teal.png';
+
+
+const images = [
+    applegreen,
+    blue,
+    brown,
+    cyan,
+    forestgreen,
+    gold,
+    green,
+    grey,
+    mossgreen,
+    purple,
+    red,
+    teal
+];
+
+export default images;
