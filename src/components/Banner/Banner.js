9c5d35d198ef1f43fc1e7fd912d88c0aaa075469
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-09-16 12:30:36 -0700

    Banner text showing thanks to Shelly

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './Banner.css';
 
 const Banner = () => (
+  <div className="row align-items-end">
+  <div className="col align-self-end">
   <div className="banner text-center d-flex align-items-center justify-content-center">
-    <p className="m-0">Click on an image to earn a point, but if you click the same image twice you lose!</p>
+   <p></p>
+   <p></p>
+    <p className="m-0 banner-paragraph">Click on an image to earn a point, but if you click the same image twice you lose!</p>
+  </div>
+  </div>
   </div>
 );
 export default Banner;
