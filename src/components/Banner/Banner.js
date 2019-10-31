b2c8e4a761305bb2e2a3eccf941260b0afab1654
Connor Dougherty <connor.j.dougherty9@gmail.com>
2019-10-31 10:05:21 -0700

    fixing image glitch

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,8 +5,7 @@ const Banner = () => (
   <div className="row align-items-end">
   <div className="col align-self-end">
   <div className="banner text-center d-flex align-items-center justify-content-center">
-   <p></p>
-   <p></p>
+ 
     <p className="m-0 banner-paragraph">Click on an image to earn a point, but if you click the same image twice you lose!</p>
   </div>
   </div>
